80a9d17f08136fee8d7c02cab56209002d915844
Steven Hunter <Steven Hunter>
2016-10-13 01:19:02 +0100

    First prototype of the game with boss, players, bullets, health UI, platforms, gamecontroller and dev console.

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,208 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossController : MonoBehaviour
+{
+    //Boss parts
+    private Rigidbody2D rB;
+    private InputController iC;
+    public GameController gC;
+    [SerializeField]
+    private Transform[] bossArm;
+    [SerializeField]
+    private Transform[] bulletSpawn;
+    [SerializeField]
+    private Transform rocketSpawn;
+
+    private bool setPlayer = false;
+
+    //Control Options
+    public int controllerNumber = 0;
+    [SerializeField]
+    private bool useKeyboard = false;
+
+    //Spawnables for attacks
+    [SerializeField]
+    private GameObject bullet;
+    [SerializeField]
+    private GameObject rocket;
+
+    //Adjustable movement variables
+    [SerializeField]
+    private float speed = 7.5f;
+
+    //Private movement variables
+    private float vertRight;
+    private float horizRight;
+    private float vertLeft;
+    private float horizLeft;
+    private float arm1TurnAmount;
+    private float arm2TurnAmount;
+
+    //Attack cooldowns
+    float bulletFireDelay = 0.25f;
+    bool canFire1 = true;
+    bool canFire2 = true;
+    float specialCoolDown = 30.0f;
+    float currentSpecialCoolDown = 30.0f;
+    bool parked = false;
+
+    float maxHealth = 1000f;
+    float currentHealth = 1000f;
+
+    void Start()
+    {
+        iC = GetComponent<InputController>();
+        rB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (iC != null && !setPlayer)
+        {
+            iC.SetPlayer(controllerNumber);
+            setPlayer = true;
+        }
+        InputController();
+    }
+
+    void InputController()
+    {
+        if (!useKeyboard) //Use XBox controller input
+        {
+            vertRight = -iC.RightVertical();
+            horizRight = iC.RightHorizontal();
+            vertLeft = -iC.LeftVertical();
+            horizLeft = iC.LeftHorizontal();
+
+            if(iC.PressedX())
+            {
+                parked = !parked;
+            }
+
+            if(!parked)
+            {
+                arm1TurnAmount = (Mathf.Atan2(horizRight, vertRight) * Mathf.Rad2Deg);
+                if (vertRight != 0 || horizRight != 0)
+                {
+                    bossArm[0].transform.eulerAngles = new Vector3(
+                        bossArm[0].transform.eulerAngles.x,
+                        bossArm[0].transform.eulerAngles.y,
+                        arm1TurnAmount - 90.0f
+                        );
+                    bossArm[1].transform.eulerAngles = new Vector3(
+                        bossArm[1].transform.eulerAngles.x,
+                        bossArm[1].transform.eulerAngles.y,
+                        arm1TurnAmount - 90.0f
+                        );
+                }
+            }
+            else
+            {
+                arm1TurnAmount = (Mathf.Atan2(horizRight, vertRight) * Mathf.Rad2Deg);
+                arm2TurnAmount = (Mathf.Atan2(horizLeft, vertLeft) * Mathf.Rad2Deg);
+                if (vertRight != 0 || horizRight != 0)
+                {
+                    bossArm[0].transform.eulerAngles = new Vector3(
+                        bossArm[0].transform.eulerAngles.x,
+                        bossArm[0].transform.eulerAngles.y,
+                        arm1TurnAmount - 90.0f
+                        );
+                    bossArm[1].transform.eulerAngles = new Vector3(
+                        bossArm[1].transform.eulerAngles.x,
+                        bossArm[1].transform.eulerAngles.y,
+                        arm2TurnAmount - 90.0f
+                        );
+                }
+            }
+            
+            if (iC.RightTrigger() > 0 && canFire1)
+            {
+                canFire1 = false;
+                StartCoroutine(HoldingRightTrigger());
+            }
+            if (iC.LeftTrigger() > 0 && canFire2)
+            {
+                canFire2 = false;
+                StartCoroutine(HoldingLeftTrigger());
+            }
+        }
+        else // the player is using a keyboard, lets follow the mouse instead
+        {
+            var objectPos = Camera.main.WorldToScreenPoint(bossArm[0].transform.position);
+            var dir = Input.mousePosition - objectPos;
+            bossArm[0].transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg));
+
+            objectPos = Camera.main.WorldToScreenPoint(bossArm[1].transform.position);
+            dir = Input.mousePosition - objectPos;
+            bossArm[1].transform.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg));
+
+            //Attack input detection
+            if (Input.GetButton("Fire1") && canFire1)
+            {
+                canFire1 = false;
+                StartCoroutine(HoldingRightTrigger());
+            }
+            if (Input.GetButton("Fire2") && canFire2)
+            {
+                canFire2 = false;
+                StartCoroutine(HoldingLeftTrigger());
+            }
+
+            //Flip drone sprite to face direction of arm
+            if (dir.x < 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else if (dir.x > 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(!parked)
+        {
+            if (!useKeyboard)
+                this.transform.Translate(new Vector2(iC.LeftHorizontal() * speed * Time.deltaTime, 0f));
+            else
+                this.transform.Translate(new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0f));
+        }
+    }
+
+    IEnumerator HoldingRightTrigger()
+    {
+        FireBullet(0);
+        yield return new WaitForSeconds(bulletFireDelay);
+        canFire1 = true;
+    }
+
+    IEnumerator HoldingLeftTrigger()
+    {
+        FireBullet(1);
+        yield return new WaitForSeconds(bulletFireDelay);
+        canFire2 = true;
+    }
+
+    void FireBullet(int armToFireFrom)
+    {
+        GameObject newBullet = Instantiate(bullet, bulletSpawn[armToFireFrom].transform.position, bossArm[armToFireFrom].transform.rotation) as GameObject;
+        newBullet.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(7f, 0f), ForceMode2D.Impulse);
+        newBullet.GetComponent<Bullet>().SetOrigin(1);
+        Destroy(newBullet, 5f);
+    }
+
+    public void ResetPlayer()
+    {
+        currentHealth = maxHealth;
+        gC.UpdatePlayerHealth(controllerNumber, maxHealth, currentHealth);
+    }
+
+    public void AdjustHealth(float amountToAdjust)
+    {
+        currentHealth += amountToAdjust;
+        gC.UpdatePlayerHealth(controllerNumber, maxHealth, currentHealth);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject developerConsole;
+    bool devConsoleActive = false;
+    public Text previousCommands;
+    public GameObject devConInputField;
+    string commandsHelp;
+
+    [SerializeField]
+    private Transform[] spawnPoint;
+    [SerializeField]
+    private Transform playerHolder;
+    [SerializeField]
+    private GameObject playerPrefab;
+    [SerializeField]
+    private GameObject bossPrefab;
+    [SerializeField]
+    Transform[] players = new Transform[4];
+
+    [SerializeField]
+    private GameObject platformHolder;
+
+    [SerializeField]
+    private Image[] currentHealth;
+
+    [SerializeField]
+    private GameObject[] winStates;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == 0)
+            {
+                GameObject boss = Instantiate(bossPrefab, spawnPoint[i].transform.position, spawnPoint[i].transform.rotation) as GameObject;
+                boss.GetComponent<BossController>().controllerNumber = (i + 1);
+                boss.GetComponent<BossController>().gC = this;
+                boss.transform.SetParent(playerHolder);
+                players[i] = boss.transform;
+            }
+            else
+            {
+                GameObject player = Instantiate(playerPrefab, spawnPoint[i].transform.position, spawnPoint[i].transform.rotation) as GameObject;
+                player.GetComponent<DroneController>().controllerNumber = (i + 1);
+                player.GetComponent<DroneController>().gC = this;
+                player.transform.SetParent(playerHolder);
+                players[i] = player.transform;
+            }
+        }
+        SetCommandsHelpString();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            if (devConsoleActive)
+            {
+                Time.timeScale = 1;
+                developerConsole.SetActive(false);
+                devConsoleActive = false;
+            }
+            else if (!devConsoleActive)
+            {
+                Time.timeScale = 0;
+                developerConsole.SetActive(true);
+                devConsoleActive = true;
+                EventSystem.current.SetSelectedGameObject(developerConsole);
+                EventSystem.current.SetSelectedGameObject(devConInputField);
+            }
+        }
+    }
+
+    public void UpdatePlayerHealth(int player, float maxHealth, float currentHealth)
+    {
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            this.players[(player - 1)].gameObject.SetActive(false);
+        }
+        this.currentHealth[(player - 1)].fillAmount = currentHealth / maxHealth;
+
+        CheckWinState();
+    }
+
+    private void CheckWinState()
+    {
+        if (currentHealth[0].fillAmount == 0)
+        {
+            winStates[1].SetActive(true);
+        }
+        if (currentHealth[1].fillAmount == 0 && currentHealth[2].fillAmount == 0 && currentHealth[3].fillAmount == 0)
+        {
+            winStates[0].SetActive(true);
+        }
+
+    }
+
+    void ResetGame(bool fly, bool twin)
+    {
+        players[0].GetComponent<BossController>().ResetPlayer();
+        players[0].gameObject.SetActive(true);
+        for (int i = 1; i < 4; i++)
+        {
+            players[i].GetComponent<DroneController>().ResetPlayer(fly, twin);
+            players[i].gameObject.SetActive(true);
+        }
+        if(fly)
+        {
+            platformHolder.SetActive(false);
+        }
+        else
+        {
+            platformHolder.SetActive(true);
+        }
+        winStates[0].SetActive(false);
+        winStates[1].SetActive(false);
+    }
+
+    public void DeveloperConsole(string command)
+    {
+        if (command == "`")
+        {
+            devConInputField.GetComponent<InputField>().text = "";
+        }
+        else
+        {
+            if (command.Contains("/help"))
+            {
+                previousCommands.text = previousCommands.text + "\n" + commandsHelp;
+            }
+            else if (command.Contains("/reset"))
+            {
+                if (command.Contains("false false"))
+                    ResetGame(false, false);
+                else if (command.Contains("false true"))
+                    ResetGame(false, true);
+                else if (command.Contains("true false"))
+                    ResetGame(true, false);
+                else if (command.Contains("true true"))
+                    ResetGame(true, true);
+                else
+                    ResetGame(false, false);
+            }
+
+            previousCommands.text = previousCommands.text + "\n" + command;
+            devConInputField.GetComponent<InputField>().text = "";
+            EventSystem.current.SetSelectedGameObject(developerConsole);
+            EventSystem.current.SetSelectedGameObject(devConInputField);
+        }
+    }
+
+    void SetCommandsHelpString()
+    {
+        commandsHelp = "reset bool bool (fly, twinstick)"// +
+            //"ui (on/off)\n" +
+            //"give (1/2/3/4) (autogun/balloon/glove/mine/monkey/oil/rocket/shield/stungun/tesla/thruster/windblower)\n" +
+            //"freecam off || freecam on\n" +
+            //"freeze (all/1/2/3/4) || unfreeze (all/1/2/3/4)\n" +
+            //"restart"
+            ;
+    }
+}
